8f0fec8cbc37a666e00bb5c63807be6b753ca7e5
YuriiPytlovanyi <pytlovanyij@gmail.com>
2025-04-20 14:38:30 +0300

    publish

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import "./App.css";
-import RestaurantMenu from "./components/Restaurant";
 import Button from "./components/Button";
-import FruitItem from "./components/FruitItem";
-import ToDoList from "./assets/ToDoList";
 import ToDoList2 from "./components/ToDoList2";
-import Cars from "./components/Cars";
 import LightDarkTheme from "./components/LightDarkTheme";
 import StopWatch from "./components/stopwatch";
 function App() {
